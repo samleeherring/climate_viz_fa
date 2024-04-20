62cc117209f820884dcdbd5c696097a74d7abf89
Sam Lee Herring <samleeherring@gmail.com>
2024-04-20 01:05:49 -0700

    Scraping the NCEI NOAA for data sets of GHCN stations closest to my hometown of Lahaina, HI to find changes in average precipitation over the last 100 years

--- code/local_weather.R
+++ code/local_weather.R
@@ -35,11 +35,11 @@ my_station <- inventory %>%
 
 ## Found this station only 9.63km from Lahaina that goes from 1919 until now
          
-station_daily <- "https://www.ncei.noaa.gov/pub/data/ghcn/daily/by_station/{my_station}.csv.gz"
+station_daily <- "https://www.ncei.noaa.gov/pub/data/ghcn/daily/by_station/USC00515715.csv.gz"
 
 local_weather <- read_csv(station_daily,
-                          col_names = c("station_id", "date", "element", "value", "m_flag", 
-                                        "q_flag", "s_flag", "time")) %>%
+                      col_names = c("station_id", "date", "element", "value", "m_flag", 
+                                    "q_flag", "s_flag", "time")) %>%
   select(date, element, value) %>%
   pivot_wider(names_from = "element", values_from = "value",
               values_fill = 0) %>%
@@ -49,10 +49,75 @@ local_weather <- read_csv(station_daily,
          TMIN = TMIN /10,
          PRCP = PRCP/10) %>%
   rename_all(tolower)
+
+#dataframe for finding the avg prcp during the wet season (Nov-Mar)
+wet_season <- local_weather %>%
+  select(date, prcp) %>%
+  mutate(month = month(date)) %>%
+  arrange(month) %>%
+  filter(month > 10 | month < 4,
+         prcp > 0) ## avg = 11.8mm
+
+#dataframe for the high avg of wet season prcp
+big_wet <- wet_season %>%
+  select(date, prcp) %>%
+  filter(prcp >= 11.8)
+
+#dataframe for finding the avg prcp during the dry season (Apr-Oct)
+dry_season <- local_weather %>%
+  select(date, prcp) %>%
+  mutate(month = month(date)) %>%
+  arrange(month) %>%
+  filter(month > 3 & month < 11,
+         prcp > 0) ## avg = 6.3mm
   
 
 local_weather %>%
-  ggplot(aes(x = date, y=prcp)) +
-  geom_line()  
+  ggplot(aes(x = date, y = prcp)) +
+  geom_line(color = "darkgreen") +
+  geom_smooth(data = wet_season,
+              aes(color = "lightblue")) +
+  geom_smooth(data = dry_season,
+              aes(color = "pink")) +
+  geom_smooth(data = big_wet,
+              aes(color = "#77C3E3")) +
+  scale_x_date(expand=c(-0,0))+
+  scale_y_continuous(breaks=seq(20, 160, 20), limits=c(0, 160), expand=c(-0,0)) +
+  scale_color_manual(name = NULL,
+                     values=c("#77C3E3", "lightblue", "pink"),
+                     labels=c("High avg", "Wet season avg",
+                              "Dry season avg")) +
+  
+  labs(
+    x = NULL,
+    y = "Precipitation (mm)",
+    title = "Daily precipitation rates for Lahaina, HI (1919 - 2024)",
+    subtitle = "Excludes 11 extreme outliers of over 150mm of daily rainfall"
+  ) +
+  
+  theme(
+    text = element_text(color = "white"),
+    plot.background = element_rect(fill = "#3B3B3B", color = "black"),
+    plot.title = element_text(face = "bold", size = 15),
+    plot.margin = margin(0.5,1,0.5,0.5, "cm"),
+    panel.background = element_rect(fill = "#FFFFF0"),
+    panel.grid.major.y = element_line(color = "darkgrey", linetype = "dotted"),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    panel.grid.minor.y = element_blank(),
+    axis.text = element_text(color="white"),
+    axis.line.x = element_blank(),
+    axis.line.y = element_blank(),
+    legend.background = element_blank(),
+    legend.key = element_blank(),
+    legend.position = "inside",
+    legend.position.inside = c(0.69, 0.89),
+    legend.text = element_text(color="black")
+  )
+
+## Yeah not really sure what happened between 2000-2005, but I do remember a bunch
+## of government strikes at the time... will investigate
+
+ggsave("figures/local_weather.png", width=7, height=4, units = "in")
 
 
