75951c65394cfc3bbfd742392249940cbb0d4014
Sam Lee Herring <samleeherring@gmail.com>
2024-04-19 23:52:21 -0700

    Code revisions for earlier plots

--- code/temperature_bar_plot.R
+++ code/temperature_bar_plot.R
@@ -10,17 +10,17 @@ annotation <- t_data %>%
   arrange(year) %>%
   slice(1, n()) %>%
   mutate(t_diff = 0,
-         x=year + c(-5, 5))
+         x=year + c(-5.5, 5.5))
 
 max_t_diff <- format(round(max(t_data$t_diff), 1), nsmall = 1)
 
 t_data %>%
   ggplot(aes(x=year, y=t_diff, fill=t_diff))+
-  geom_col(show.legend = FALSE)+
+  geom_col(aes(x=year, y=t_diff, fill=t_diff))+
   geom_text(data = annotation, aes(x=x, label = year), color="white")+
   geom_text(x=1880, y=1, hjust=0,
             label=glue("Global temperatures have increased by over {max_t_diff}\u00B0C since {min(t_data$year)}"),
-            color="white")+
+            color="white", size = 4.3)+
   #scale_fill_gradient2(low="darkblue", mid="white", high="darkred",
   #                     midpoint=0, limits =c(-0.5, 1.5))+
   #scale_fill_gradientn(colors=c("darkblue", "white", "darkred"),
@@ -29,11 +29,15 @@ t_data %>%
   scale_fill_stepsn(colors=c("darkblue", "white", "darkred"),
                     values = rescale(c(min(t_data$t_diff), 0, max(t_data$t_diff))),
                     limits = c(min(t_data$t_diff), max(t_data$t_diff)),
-                    n.breaks=9)+
+                    n.breaks=9, guide = guide_colorbar(frame.colour = "white",
+                                                       frame.linewidth = 0.3))+
+  ## decided to add in a legend for scale and reposition things better
   theme_void()+
   theme(
     plot.background = element_rect(fill="black"),
-    legend.text = element_text(color="white")
+    legend.text = element_text(color="white"),
+    legend.position = "inside",
+    legend.position.inside = c(0.12, 0.6)
   )
 
 ## played around with scale_fill functions in lines 24-32 to find which best mimicked
--- code/temperature_index_plot.R
+++ code/temperature_index_plot.R
@@ -5,29 +5,30 @@ read_csv("data/GLB.Ts+dSST.csv", skip=1, na ="***")%>%
   select(year = Year, t_diff = `J-D`) %>%
   ggplot(aes(x = year, y = t_diff))+
   geom_line(aes(color="1"), size=0.5, show.legend = FALSE)+
-  geom_point(fill="white", color="grey", shape=21, show.legend = TRUE)+
+  geom_point(fill="white", aes(color="1"), shape=21, show.legend = TRUE)+
   geom_smooth(se=FALSE, aes(color="2"), size=0.5, span=0.15, show.legend = FALSE)+
   scale_x_continuous(breaks=seq(1880, 2024, 20), expand=c(0,0))+
   scale_y_continuous(limits=c(-0.5, 1.5), expand=c(-0,0))+
   scale_color_manual(name=NULL,
-                     values=c("grey", "black"),
+                     breaks = c(1,2),
+                     values=c("darkgrey", "black"),
                      labels=c("Annual mean", "Lowess smoothing"),
-                     guide = guide_legend(override.aes = list(shape=15, size = 5, 
-                     color= "1", "2"))
+                     guide = guide_legend(override.aes = list(shape=15, size = 5))
                      )+
 ## I cannot figure out how to get the legend boxes to be different colors,
 ## Probably something in line 9 or 16
+## *20 day later review: figured it out, forgot to map color to aes in line 9
   labs(
-    x = "YEAR",
-    y = "Temperature anomaly (C)",
+    x = "Year",
+    y = "Temperature anomaly (\u00B0C)",
     title = "GLOBAL LAND-OCEAN TEMPERATURE INDEX",
-    subtitle = "Data source: NASA's Goddard Institute for Space Studies (GISS).
-    \n Credit: NASA/GISS")+
+    subtitle = "Data source: NASA's Goddard Institute for Space Studies (GISS)
+    \nCredit: NASA/GISS")+
   theme_light()+
   theme(
     axis.ticks = element_blank(),
     plot.title.position = "plot",
-    plot.title = element_text(margin = margin(b=10), color="red", face="bold"),
+    plot.title = element_text(margin = margin(b=10), color="darkred", face="bold"),
     plot.subtitle = element_text(size=8, margin = margin(b=10)),
     legend.position = c(0.15, 0.9),
     legend.title = element_text(size=0),
