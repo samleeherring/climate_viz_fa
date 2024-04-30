613e742ad352799626a72b367c90f8cab46f5b3f
Sam Lee Herring <samleeherring@gmail.com>
2024-04-29 19:19:13 -0700

    Fixed the legend and color scale issues by getting rid of discrete variable is_this_year and adding on secondary plot frame

--- code/maui_prcp_data.R
+++ code/maui_prcp_data.R
@@ -9,28 +9,15 @@ library(scales)
 this_year <- year(today())
 this_month <- month(today(),label = TRUE, abbr = FALSE)
 this_day <- ordinal(day(today()))
+today <- today()
 
-last_dec <- local_weather2 %>%
-  filter(month(date) == '12') %>%
-  mutate(year = year + 1,
-         month = "last_Dec")
-
-next_jan <-local_weather2 %>%
-  filter(month(date) == '1') %>%
-  mutate(year = year - 1,
-         month = "next_Jan")
-
-annotation <- local_weather2 %>%
+annotation <- west_maui_prcp %>%
   slice_max(year(date)) %>%
   slice_max(month(date)) %>%
-  slice_max(day(date))
+  slice_max(day(date)) 
 
-local_weather3 <- bind_rows(last_dec, local_weather2, next_jan) %>%
-  mutate(month = factor(month, levels = c("last_Dec", month.abb, "next_Jan")),
-         month_number = as.numeric(month) - 1,
-         this_year = year == 2024) 
 
-local_weather2 <- local_weather %>%
+west_maui_prcp <- local_weather %>%
   select(date, prcp) %>%
   mutate(year = year(date),
          month = as.character(month(date)),
@@ -38,41 +25,49 @@ local_weather2 <- local_weather %>%
          is_this_year = year ==this_year) %>%
   filter(year != 1919) %>%
   group_by(year) %>%
-  mutate(cum_prcp = cumsum(prcp)) %>%
+  drop_na(prcp) %>%
+  mutate(cum_prcp = cumsum(prcp)) %>% 
   ungroup() %>%
   mutate(new_date = ymd(glue("2024-{month}-{day}"))) 
 
   
-local_weather2 %>%
+west_maui_prcp %>%
+  filter(year == 2024 & month(date) == 2) %>%
+  arrange(-day(date)) %>%
+  print(n=20)
+
+## This plot has stopped working because cum_sum(prcp) stopped calculating
+## Figured it out: prcp stopped being recorded causing cum_prcp to return NA
+
+# m <- matrix(sample(c(NA, 1:10), 100, replace = TRUE), 10)
+# d <- as.data.frame(m)
+# d[is.na(d)] <- 0
+
+
+w <- west_maui_prcp %>%
   ggplot(aes(x = new_date, y = cum_prcp, group = year,
-             color = is_this_year, size = is_this_year)) +
+             size = is_this_year)) +
     geom_line(aes(x = new_date, y = cum_prcp, group = year,
                 color = is_this_year, size = is_this_year), show.legend = FALSE) +
     geom_text(data = annotation,
             aes(x = new_date, y = cum_prcp, label = glue('{year}'),
                 color = is_this_year),
             inherit.aes = FALSE, vjust = -0.7, size =4)+
-    geom_smooth(aes(group = 1), color = "black", size = 0.3,
-              method ='gam', formula = y ~ s(x, bs = "cs")) +
-    # geom_text(data = NULL, aes(label = 'Overall average', color = 'black'),
-    #           inherit.aes = TRUE) +
+
+    scale_x_date(date_breaks = '2 months',
+                 date_labels = '%B', expand = c(0,0)) +
     scale_y_continuous(breaks = seq(0, 2200, 300),
                      labels = seq(0, 220, 30),
                      expand = c(0, 0)) +
     scale_color_manual(name = NULL,
                      breaks = c(F, T),
                      values = c("lightgrey", "dodgerblue"),
-                     labels = c("Past years", "Current year"),
+                     labels = c("Past years", "Overall average"),
                      guide = guide_legend(override.aes =list(label = "",
-                                                        shape=18, size=10,
+                                                        shape=15, size=5,
                                                         fill = c("lightgrey",
                                                                  "dodgerblue")))
                      ) +
-    # guides(
-    #   fill = guide_legend(
-    #     title = "Legend Title",
-    #     override.aes = aes(label = "b", shape=18, size=5))) +
-  ## So it looks like guides() does actually nothing whatsoever. Sick.
   
     scale_size_manual(breaks = c(F, T),
                     values = c(0.3, 1)) +
@@ -99,8 +94,22 @@ local_weather2 %>%
       legend.title = element_text(color="black")
     )
 
+legend <- data.frame(new_date = today - 6, cum_prcp = c(190, 165),
+                     color = c('lightgrey', 'black'), size = 5)
+w +
+  geom_point(data = legend, aes(x = new_date, y = cum_prcp, color = after_scale(c('lightgrey', 'black'))),
+             shape = 15, size = 0.1, group = 1) +
+  geom_smooth(aes(group = 1), color = "black", size = 0.3,
+              method ='gam', formula = y ~ s(x, bs = "cs")) 
 
 ggsave("figures/cumulative_prcp.png", width = 6, height = 5, units = "in")
+
+# guides(
+#   fill = guide_legend(
+#     title = "Legend Title",
+#     override.aes = aes(label = "b", shape=18, size=5))) +
+## So it looks like guides() does actually nothing whatsoever. Sick.
+
 
 ## Gonna try some more plotting exercises since I went through all the trouble
 ## of finding this data
