65e352968e95bad5ab1e18db6f19df8aaf2ff1ff
Sam Lee Herring <samleeherring@gmail.com>
2024-05-12 18:15:52 -0700

    animated gif showing change of global temp anomaly over time

--- code/global_temp_anomaly.R
+++ code/global_temp_anomaly.R
@@ -4,6 +4,7 @@ library(ncdf4)
 library(data.table)
 library(lubridate)
 library(ggridges)
+library(gganimate)
 
 ## Run only once then comment out
 # url <- "https://data.giss.nasa.gov/pub/gistemp/gistemp250_GHCNv4.nc.gz"
@@ -81,10 +82,54 @@ t_data %>%
   guides(fill = guide_colorbar(title.position = "top",
                                title.hjust = 0.6))
 
+ggsave("figures/global_temp_anomaly_raster.png", width = 10, height = 4.7,
+       units = "in")
 
-ggsave("figures/global_temp_anomaly_raster.png", width = 10, height = 4.7, units = "in")
-  
-  
 
+
+
+g_a <- t_data %>%
+  mutate(t_diff = case_when(t_diff < -4 ~ -4,
+                            t_diff > 4 ~ 4,
+                            TRUE ~ t_diff)) %>%
+  #filter(year == 2000) %>%
+  ggplot(aes(x=longitude, y=lattitude, fill=t_diff))+
+  geom_raster(interpolate = TRUE) +
+  scale_fill_gradient2(name = "Anomaly (\u00B0C)",
+                       low = "darkblue", mid = "white", high = "darkred",
+                       midpoint = 0,
+                       limits = c(-5, 5),
+                       breaks = c(-4, -2, 0 , 2, 4)) +
+  coord_fixed(expand=FALSE) +
+  
+  labs(x = NULL,
+       y = NULL,
+       title = "Global annual land temperature anomalies 1950-2024"
+  ) +
+  
+  theme_void() +
+  theme(
+    plot.background = element_rect(fill = "black", color = "black"),
+    plot.title = element_text(color = "white"),
+    panel.background = element_rect(fill = "darkblue"),
+    panel.grid = element_blank(),
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    strip.text.x = element_blank(),
+    strip.text.y.left = element_text(angle=0, color = "white"),
+    strip.background = element_blank(),
+    legend.position = c(0.16, 0.18),
+    legend.direction = "horizontal",
+    legend.title = element_text(color="white", size = 8),
+    legend.background = element_rect(fill=NA, color = NA),
+    legend.text = element_text(color="white", size = 6),
+  ) +
+  guides(fill = guide_colorbar(title.position = "top",
+                               title.hjust = 0.6)) +
+  transition_manual(frames = year, cumulative = FALSE)
+  
+  animate(g_a, width=6, height=3.17, units="in", res = 300) 
+
+  anim_save('figures/global_temp_anomaly_gif.gif')
 
 
